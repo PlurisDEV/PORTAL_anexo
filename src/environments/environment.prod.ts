41fe295a936905e111c8ef93a92396ef3589f769
rnunes <suportecrm@plurismidia.com.br>
2026-09-21 15:28:00 -0300

    Alterando environment de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  urlBackend: '',
-  clientId: '',
+  urlBackend: 'https://portalanexobackend.ranbaxy.plurismidia.com.br',
+  clientId: 'portal-anexo-pluris',
   clientSecret: '',
 
   obterTokenURL: '/oauth/token',
